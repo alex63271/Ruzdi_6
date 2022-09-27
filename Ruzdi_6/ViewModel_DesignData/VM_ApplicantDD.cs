214f53f914f24f9b49fc5575ee1f697158cf2c1f
Алексей Новосельцев <alex6327@yandex.ru>
2022-09-27 09:00:14 +0300

    Добавлен шаблон для дизайнера xaml в WindowForUZ1

--- Ruzdi_6/ViewModel_DesignData/VM_ApplicantDD.cs
+++ Ruzdi_6/ViewModel_DesignData/VM_ApplicantDD.cs
@@ -1,4 +1,5 @@
 ﻿using Ruzdi_6.Model.Applicant_Classes;
+using Ruzdi_6.Model.Other_Classes;
 
 namespace Ruzdi_6.ViewModel_DesignData
 {
@@ -74,6 +75,51 @@ namespace Ruzdi_6.ViewModel_DesignData
             ApplicantList.Add(ApplicantPledgee);
             */
             #endregion
+            SelectedApplicant = new NotificationApplicant
+            {
+                Role = 2,
+                Organization = new ApplicantOrganization
+                {
+                    NameFull = "NameFull",
+                    UINN = "INN",
+                    URN = "OGRN",
+                    Email = ""
+                },
+                Attorney = new ApplicantAttorney
+                {
+                    Name = new ApplicantAttorneyName
+                    {
+                        First = "First",
+                        Last = "Last",
+                        Middle = "Middle"
+                    },
+                    BirthDate = DateTime.Now,
+                    Authority = "основания полномочий",
+                    PersonDocument = new ApplicantAttorneyPersonDocument
+                    {
+                        Code = 21,
+                        Name = "Паспорт",
+                        SeriesNumber = "1234567890"
+                    },
+                    PersonAddress = new ApplicantAttorneyPersonAddress
+                    {
+                        AddressRF = new ApplicantAttorneyPersonAddressAddressRF
+                        {
+                            registration = true,
+                            Region = "Москва",
+                            District = "район",
+                            City = "Москва",
+                            Locality = "Нас. пункт",
+                            Street = "улица",
+                            House = "Дом",
+                            Building = "строение",
+                            Apartment = "квартира",
+                        }
+                    }
+                }
+            };
+
+
 
             DisplayApplicant = new NotificationApplicant
             {
@@ -118,8 +164,11 @@ namespace Ruzdi_6.ViewModel_DesignData
                     }
                 }
             };
+
         }
 
         public NotificationApplicant DisplayApplicant { get; set; }
+
+        public NotificationApplicant SelectedApplicant { get; set; }
     }
 }
